b8913ae3a9250bce3dcd1d62d493c22ce239b6c5
NathanDevelops97 <you@example.com>
2016-10-03 14:51:57 -0400

    Gameover controller code sorted

--- SingleViewAppSwift2Template/GameOverViewController.swift
+++ SingleViewAppSwift2Template/GameOverViewController.swift
@@ -9,9 +9,17 @@
 import UIKit
 
 class GameOverViewController: UIViewController {
+    
+    
+    var roundsCorrect = 0
+    var roundsPlayed = 0
+    let totalRounds = 6
+    
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        showScore()
 
         // Do any additional setup after loading the view.
     }
@@ -23,13 +31,19 @@ class GameOverViewController: UIViewController {
     
     
     
+    @IBOutlet weak var gameOverScore: UILabel!
+    
+    
+    func showScore() {
+        gameOverScore.text = "\(roundsCorrect)/\(totalRounds)"
+    }
     
     
     
-    
-    
-    
-    
+    @IBAction func PlayAgain(_ sender: AnyObject) {
+        dismiss(animated: true, completion: nil)
+
+    }
     
     
     
